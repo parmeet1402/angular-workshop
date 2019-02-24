c63f66c26fc1074e2d62c3325b1d448ce8f50985
Parmeet <parmeet.asi@gmail.com>
2019-02-24 10:34:23 +0530

    init commit

--- test-app/src/app/app-routing.module.ts
+++ test-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/view/home/home.component';
+import { AboutComponent } from './components/view/about/about.component';
+import { ProjectComponent } from './components/view/project/project.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'projects', component: ProjectComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- test-app/src/app/app.module.ts
+++ test-app/src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/commons/header/header.component';
+import { FooterComponent } from './components/commons/footer/footer.component';
+import { AboutComponent } from './components/view/about/about.component';
+import { ProjectComponent } from './components/view/project/project.component';
+import { HomeComponent } from './components/view/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ProjectComponent,
+    HomeComponent
+
   ],
   imports: [
     BrowserModule,
